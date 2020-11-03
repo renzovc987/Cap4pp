7cf754dd89084122a36549c8e31893fdde3c3636
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-11-02 22:11:33 -0500

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,28 @@
+#include <cstdint> // uint64_t
+#include <vector> // std::vector
+#include <thread> // std::thread
+// this function will be called by the threads (should be void)
+void say_hello(uint64_t id) {
+ std::cout << "Hello from thread: " << id << std::endl;
+}
+
+ // this runs in the master thread
+ int main(int argc, char * argv[]) {
+
+	 const uint64_t num_threads = 4;
+	 std::vector<std::thread> threads;
+
+	 // for all threads
+	 for (uint64_t id = 0; id < num_threads; id++)
+	 // emplace the thread object in vector threads
+	 // using argument forwarding, this avoids unnecessary
+	 // move operations to the vector after thread creation
+	 	threads.emplace_back(
+	 // call say_hello with argument id
+	 	say_hello, id
+	 );
+
+	 // join each thread at the end
+	 for (auto& thread: threads)
+	  thread.join();
+ }
