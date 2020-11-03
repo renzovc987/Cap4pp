9980a350ec1c290b794308000360200787edc67d
Renzo Vicente Castro <38012028+renzovc987@users.noreply.github.com>
2020-11-03 08:58:20 -0500

    Update fibotw.cpp

--- fibotw.cpp
+++ fibotw.cpp
@@ -6,38 +6,31 @@
  template <typename value_t, typename index_t>
  void fibo(
  value_t n,
- value_t * result) { // <- here we pass the address
-
+ value_t * result) {
 	 value_t a_0 = 0;
 	 value_t a_1 = 1;
 
 	 for (index_t index = 0; index < n; index++) {
 	 	const value_t tmp = a_0; a_0 = a_1; a_1 += tmp;
 	 }
-
- 	*result = a_0; // <- here we write the result
+ 	*result = a_0;
  }
 
- // this runs in the master thread
  int main(int argc, char * argv[]) {
 
 	 const uint64_t num_threads = 32;
 	 std::vector<std::thread> threads;
-
-	 // allocate num_threads many result values
 	 std::vector<uint64_t> results(num_threads, 0);
 
 	 for (uint64_t id = 0; id < num_threads; id++)
-	 threads.emplace_back(
-	 // specify template parameters and arguments
-	 fibo<uint64_t, uint64_t>, id, &(results[id])
-	 );
+		 threads.emplace_back(
+		 fibo<uint64_t, uint64_t>, id, &(results[id])
+		 );
 
-	 // join the threads
 	 for (auto& thread: threads)
-	 thread.join();
-
-	 // print the result
+	 	thread.join();
 	 for (const auto& result: results)
-	 std::cout << result << std::endl;
+	 	std::cout << result << std::endl;
  }
+
+
